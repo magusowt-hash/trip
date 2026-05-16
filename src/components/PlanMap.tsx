1c65ab82b33421c6d79073282cc0f43aa1101989
magusowt-hash <test@test.com>
2026-05-16 18:55:59 +0800

    fix(maps): update center to 33.37N 104.96E

--- src/components/PlanMap.tsx
+++ src/components/PlanMap.tsx
@@ -151,7 +151,7 @@ export default function PlanMap({
           if (window.AMap) {
             const map = new window.AMap.Map(mapRef.current, {
               zoom: 4,
-              center: [108.923611, 34.540833],
+              center: [104.9572932, 33.3721445],
               mapStyle: 'amap://styles/normal',
               viewMode: '2D',
               resizeEnable: true,
